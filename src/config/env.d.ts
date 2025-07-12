d38207f0132c7c0a2ded67d67a6fb922572ccc0b
Michael Chukwuka Meninwa <chukwuka4christ@yahoo.com>
2025-07-12 15:57:57 -0400

    Fix Firebase env import and config usage

---
+++ src/config/env.d.ts
@@ -0,0 +1,10 @@
+declare module 'src/config/env' {
+  export const firebaseEnv: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+  };
+}
